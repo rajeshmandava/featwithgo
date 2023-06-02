e290fefb3a2f159a68e6925ad65d99dbd73235d4
rajesh mandava <rajesh.mandava07@gmail.com>
2023-06-02 05:35:22 +0200

    Book Ticket logic

--- hello/hello.go
+++ hello/hello.go
@@ -6,19 +6,39 @@ import (
 )
 
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	var conferenceName string= "Go Conference"
+	const conferenceTickets int= 50
+	var remainingTickets uint= 50
 
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
-	var userTickets int
+	var firstName string
+	var lastName string
+	var userTickets uint
+	var email string
+	
+	fmt.Println("Enter your first name:")
+	fmt.Scan(&firstName)
 
-	userName = "Tom"
-	userTickets = 2
-	fmt.Printf("user %v booked %v tickets.\n", userName, userTickets)
+	fmt.Println("Enter your second name:")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address:")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets:")
+	fmt.Scan(&userTickets)
+
+
+	remainingTickets = remainingTickets - userTickets
+
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets,email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+	  
+
 }
